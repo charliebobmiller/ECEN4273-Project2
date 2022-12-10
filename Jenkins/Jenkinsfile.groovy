19606dbd87438fc211de52d2946b6bda6fabaaab
sivauerbach <114685221+sivauerbach@users.noreply.github.com>
2022-12-10 03:23:52 -0600

    Update Jenkinsfile.groovy

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
     stages {
         stage('Tests') {
             steps {
+                sh 'pip install torch --yes'
                 sh  'python3 -m pytest --junitxml build/report.xml tests/ || true'
                 junit 'build/report.xml'
             }
